8adefa01edde4da339036043138244ad6b1d0caa
Duka <slechslayer@gmail.com>
2026-03-03 17:51:26 -0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
     proxy: {
-      '/api': 'http://127.0.0.1:5098'
+      '/api': 'https://private-notes-chi.vercel.app'
     }
   }
 });
